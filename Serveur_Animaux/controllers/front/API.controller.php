b5da5bc195f903528a63939556d2b9b914f0d2c0
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-18 01:45:39 +0100

    Transformer les datas des animaux

--- Serveur_Animaux/controllers/front/API.controller.php
+++ Serveur_Animaux/controllers/front/API.controller.php
@@ -13,17 +13,61 @@ class APIController
     public function getAnimaux()
     {
         $animaux = $this->apiManager->getDBAnimaux();
+        $tabResultat = $this->formatDataLignesAnimaux($animaux);
         echo "<pre>";
-        print_r($animaux);
+        print_r($tabResultat);
         echo "</pre>";
+
+        Model::sendJSON($tabResultat);
     }
     public function getAnimal($idAnimal)
     {
-        $ligneAnimal = $this->apiManager->getDBAnimal($idAnimal);
+        $lignesAnimal = $this->apiManager->getDBAnimal($idAnimal);
+        $tabResultat = $this->formatDataLignesAnimaux($lignesAnimal);
         echo "<pre>";
-        print_r($ligneAnimal);
+        print_r($tabResultat);
         echo "</pre>";
+        Model::sendJSON($tabResultat);
     }
+
+
+    // fonction permettant de retravailler le data pour mettre chacune de ligne à tour de role(derniere etape API REST)
+    private function formatDataLignesAnimaux($lignes)
+    {
+        $tab = [];
+
+        foreach ($lignes as $ligne) {
+            // Si l'animal n'est pas ds $tab on ajoute
+            if (!array_key_exists($ligne['animal_id'], $tab)) {
+                $tab[$ligne['animal_id']] = [
+                    "id" => $ligne['animal_id'],
+                    "nom" => $ligne['animal_nom'],
+                    "description" => $ligne['animal_description'],
+                    "image" => URL . "public/images/" . $ligne['animal_image'],
+                    "famille" => [
+                        "idFamille" => $ligne['famille_id'],
+                        "libelleFamille" => $ligne['famille_libelle'],
+                        "descriptionFamille" => $ligne['famille_description']
+                    ]
+                ];
+            }
+
+            $tab[$ligne['animal_id']]['continents'][] = [
+                "idContinent" => $ligne['continent_id'],
+                "libelleContinent" => $ligne['continent_libelle']
+            ];
+        }
+
+        return $tab;
+    }
+
+
+
+
+
+
+
+
     public function getContinents()
     {
         $continents = $this->apiManager->getDBContinents();
